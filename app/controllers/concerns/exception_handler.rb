0e0b25e0af50636811cc2d4290b447a96d0a00d3
Felipe de Araujo <oifelipearaujo@gmail.com>
2017-04-17 12:03:14 -0300

    Adds auxiliar classes

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,35 @@
+module ExceptionHandler
+  extend ActiveSupport::Concern
+
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
+  included do
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      render json: {message: e.message}, status: :not_found
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      render json: {message: e.message}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    # JSON response with message; Status code 422 - unprocessable entity
+    def four_twenty_two(e)
+      render json: { message: e.message }, status: :unprocessable_entity
+    end
+
+    # JSON response with message; Status code 401 - Unauthorized
+    def unauthorized_request(e)
+      render json: { message: e.message }, status: :unauthorized
+    end
+
+end
