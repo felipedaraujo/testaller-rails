720fb5340c6cf074a75676fae99ba4444e67f924
Felipe de Araujo <daraujofelipe@gmail.com>
2017-04-21 06:44:15 -0300

    Updates cross authorization

--- config/application.rb
+++ config/application.rb
@@ -18,11 +18,16 @@ module TestallerRails
   class Application < Rails::Application
     config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => [:get, :post, :put, :options]
+    #   end
+    # end
+
+    config.action_dispatch.default_headers.merge!({
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    })
   end
 end
